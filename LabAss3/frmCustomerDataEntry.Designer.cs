b82c8ef147adf2f08d7695deb50a4ddcb583899c
xrj777 <100856625+xrj777@users.noreply.github.com>
2022-04-22 18:00:21 +0800

    Add files via upload

---
+++ LabAss3/frmCustomerDataEntry.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace LabAss3
+{
+    partial class frmCustomerDataEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.radioMale = new System.Windows.Forms.RadioButton();
+            this.radioFemale = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkReading = new System.Windows.Forms.CheckBox();
+            this.chkPainting = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioUnmarried = new System.Windows.Forms.RadioButton();
+            this.radioMarried = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbCountry = new System.Windows.Forms.ComboBox();
+            this.dtgCustomer = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lblID = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgCustomer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Country";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(245, 44);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(129, 25);
+            this.txtName.TabIndex = 2;
+            // 
+            // radioMale
+            // 
+            this.radioMale.AutoSize = true;
+            this.radioMale.Location = new System.Drawing.Point(21, 50);
+            this.radioMale.Name = "radioMale";
+            this.radioMale.Size = new System.Drawing.Size(60, 19);
+            this.radioMale.TabIndex = 4;
+            this.radioMale.TabStop = true;
+            this.radioMale.Text = "Male";
+            this.radioMale.UseVisualStyleBackColor = true;
+            // 
+            // radioFemale
+            // 
+            this.radioFemale.AutoSize = true;
+            this.radioFemale.Location = new System.Drawing.Point(198, 50);
+            this.radioFemale.Name = "radioFemale";
+            this.radioFemale.Size = new System.Drawing.Size(76, 19);
+            this.radioFemale.TabIndex = 5;
+            this.radioFemale.TabStop = true;
+            this.radioFemale.Text = "Female";
+            this.radioFemale.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioMale);
+            this.groupBox1.Controls.Add(this.radioFemale);
+            this.groupBox1.Location = new System.Drawing.Point(47, 140);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(370, 100);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gender";
+            // 
+            // chkReading
+            // 
+            this.chkReading.AutoSize = true;
+            this.chkReading.Location = new System.Drawing.Point(206, 272);
+            this.chkReading.Name = "chkReading";
+            this.chkReading.Size = new System.Drawing.Size(85, 19);
+            this.chkReading.TabIndex = 7;
+            this.chkReading.Text = "Reading";
+            this.chkReading.UseVisualStyleBackColor = true;
+            // 
+            // chkPainting
+            // 
+            this.chkPainting.AutoSize = true;
+            this.chkPainting.Location = new System.Drawing.Point(341, 273);
+            this.chkPainting.Name = "chkPainting";
+            this.chkPainting.Size = new System.Drawing.Size(93, 19);
+            this.chkPainting.TabIndex = 8;
+            this.chkPainting.Text = "Painting";
+            this.chkPainting.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Hobbies";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioUnmarried);
+            this.groupBox2.Controls.Add(this.radioMarried);
+            this.groupBox2.Location = new System.Drawing.Point(47, 341);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(370, 105);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Status";
+            // 
+            // radioUnmarried
+            // 
+            this.radioUnmarried.AutoSize = true;
+            this.radioUnmarried.Location = new System.Drawing.Point(198, 53);
+            this.radioUnmarried.Name = "radioUnmarried";
+            this.radioUnmarried.Size = new System.Drawing.Size(100, 19);
+            this.radioUnmarried.TabIndex = 1;
+            this.radioUnmarried.TabStop = true;
+            this.radioUnmarried.Text = "Unmarried";
+            this.radioUnmarried.UseVisualStyleBackColor = true;
+            // 
+            // radioMarried
+            // 
+            this.radioMarried.AutoSize = true;
+            this.radioMarried.Location = new System.Drawing.Point(21, 53);
+            this.radioMarried.Name = "radioMarried";
+            this.radioMarried.Size = new System.Drawing.Size(84, 19);
+            this.radioMarried.TabIndex = 0;
+            this.radioMarried.TabStop = true;
+            this.radioMarried.Text = "Married";
+            this.radioMarried.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 480);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 34);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Preview";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // cmbCountry
+            // 
+            this.cmbCountry.FormattingEnabled = true;
+            this.cmbCountry.Items.AddRange(new object[] {
+            "Bangladesh",
+            "1"});
+            this.cmbCountry.Location = new System.Drawing.Point(245, 90);
+            this.cmbCountry.Name = "cmbCountry";
+            this.cmbCountry.Size = new System.Drawing.Size(129, 23);
+            this.cmbCountry.TabIndex = 12;
+            // 
+            // dtgCustomer
+            // 
+            this.dtgCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgCustomer.Location = new System.Drawing.Point(663, 93);
+            this.dtgCustomer.Name = "dtgCustomer";
+            this.dtgCustomer.RowHeadersWidth = 51;
+            this.dtgCustomer.RowTemplate.Height = 27;
+            this.dtgCustomer.Size = new System.Drawing.Size(315, 150);
+            this.dtgCustomer.TabIndex = 13;
+            this.dtgCustomer.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgCustomer_CellClick);
+            this.dtgCustomer.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgCustomer_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(261, 480);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 34);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(95, 547);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 34);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(73, 557);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(0, 15);
+            this.lblID.TabIndex = 16;
+            this.lblID.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(261, 547);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(142, 34);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // frmCustomerDataEntry
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1365, 622);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dtgCustomer);
+            this.Controls.Add(this.cmbCountry);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chkPainting);
+            this.Controls.Add(this.chkReading);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmCustomerDataEntry";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmCustomerDataEntry_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgCustomer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.RadioButton radioMale;
+        private System.Windows.Forms.RadioButton radioFemale;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkReading;
+        private System.Windows.Forms.CheckBox chkPainting;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioUnmarried;
+        private System.Windows.Forms.RadioButton radioMarried;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbCountry;
+        private System.Windows.Forms.DataGridView dtgCustomer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ LabAss3/frmCustomerDataEntry.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabAss3
+{
+    public partial class frmCustomerDataEntry : Form
+    {
+        public frmCustomerDataEntry()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            string Gender, Hobby, Status = "";
+
+            if (radioMale.Checked) Gender = "male";
+            else Gender = "Female";
+            if (chkReading.Checked) Hobby = "reading";
+            else Hobby = "Painting";
+            if (radioMarried.Checked) Status = "Married";
+            else Status = "Unmarried";
+
+            frmCustomerDataEntry objPreview = new frmCustomerDataEntry();
+            objPreview.SetValues(txtName.Text, cmbCountry.Text, Gender, Hobby, Status);
+
+            try
+            {
+                frmCustomerPreview objVal= new frmCustomerPreview();
+                objVal.CheckCustomerName(txtName.Text);
+
+                frmCustomerPreview objrPreview = new frmCustomerPreview();
+                objPreview.SetValues(txtName.Text, cmbCountry.Text, Gender, Hobby, Status);
+                objPreview.Show();
+            }
+            catch (Exception ex)
+            { MessageBox.Show(ex.Message.ToString()); }
+        }
+        
+        
+        private void SetValues(string text1, string text2, string gender, string hobby, string status)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void frmCustomerDataEntry_Load(object sender, EventArgs e)
+        {
+            loadCustomer();
+        }
+        private void loadCustomer()
+        {
+
+
+            string strConnection = "Data Source=LAPTOP-76G8DFAG;Initial Catalog=CustomerDB;Persist Security Info=True;User ID=sa;Pooling=False";
+            SqlConnection objConnection = new SqlConnection(strConnection);
+            objConnection.Open();
+
+            string strCommand = "Select * from  Customer";
+            SqlCommand objCommand = new SqlCommand(strCommand, objConnection);
+
+            DataSet objDataset = new DataSet();
+            SqlDataAdapter objAdapter = new SqlDataAdapter(objCommand);
+            objAdapter.Fill(objDataset);
+            dtgCustomer.DataSource = objDataset.Tables[0];
+
+            objConnection.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string Gender, Hobby, Status = "";
+            if (radioMale.Checked) Gender = "Male";
+            else Gender = "Female";
+            if (chkPainting.Checked) Hobby = "Reading";
+            else Hobby = "Painting";
+            if (radioMarried.Checked) Status = "1";
+            else Status = "0";
+
+            string strConnection = "Data Source=LAPTOP-76G8DFAG;Initial Catalog=CustomerDB;Integrated Security=True;Pooling=False";
+            SqlConnection objConnection = new SqlConnection(strConnection);
+            objConnection.Open();
+
+            string strCommand = "insert into Customer(CustomerName,Country,Gender,Hobby,Married)Values('" + txtName.Text + "','" + cmbCountry.Text + "','" + Gender + "','" + Hobby + "'," + Status + ")";
+            SqlCommand objCommand = new SqlCommand(strCommand, objConnection);
+            objCommand.ExecuteNonQuery();
+
+            objConnection.Close();
+            loadCustomer();
+        }
+        private void clearForm()
+        {
+            txtName.Text = "";
+            cmbCountry.Text = "";
+            radioMale.Checked = false;
+            radioMarried.Checked = false;
+            radioFemale.Checked = false;
+            radioUnmarried.Checked = false;
+            chkPainting.Checked = false;
+            chkReading.Checked = false;
+        }
+        private void dtgCustomer_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            clearForm();
+            string id = dtgCustomer.Rows[e.RowIndex].Cells[0].Value.ToString();
+            displayCustomer(id);
+
+        }
+        private void displayCustomer(string id)
+        {
+            string strConnection = "Data Source=LAPTOP-76G8DFAG;Initial Catalog=CustomerDB;Integrated Security=True;Pooling=False";
+            SqlConnection objConnection = new SqlConnection(strConnection);
+            objConnection.Open();
+
+            string strCommand = "Select * from Customer where id =" + id;
+            SqlCommand objCommand = new SqlCommand(strCommand, objConnection);
+
+            DataSet objDataset = new DataSet();
+            SqlDataAdapter objAdapter = new SqlDataAdapter(objCommand);
+            objAdapter.Fill(objDataset);
+            objConnection.Close();
+            lblID.Text = objDataset.Tables[0].Rows[0][0].ToString().Trim();
+            txtName.Text = objDataset.Tables[0].Rows[0][1].ToString().Trim();
+            cmbCountry.Text = objDataset.Tables[0].Rows[0][2].ToString().Trim();
+            string Gender = objDataset.Tables[0].Rows[0][3].ToString().Trim();
+            if (Gender.Equals("Male")) radioMale.Checked = true;
+            else radioFemale.Checked = true;
+            string Hobby = objDataset.Tables[0].Rows[0][4].ToString().Trim();
+            if (Hobby.Equals("Reading")) chkReading.Checked = true;
+            else chkPainting.Checked = true;
+            string Married = objDataset.Tables[0].Rows[0][5].ToString().Trim();
+            if (Married.Equals("True")) radioMarried.Checked = true;
+            else radioUnmarried.Checked = true;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string Gender, Hobby, Status = "";
+            if (radioMale.Checked) Gender = "Male";
+            else Gender = "Female";
+            if (chkReading.Checked) Hobby = "Reading";
+            else Hobby = "Painting";
+            if (radioMarried.Checked) Status = "1";
+            else Status = "0";
+
+            string strConnection = "Data Source=LAPTOP-76G8DFAG;Initial Catalog=CustomerDB;Integrated Security=True;Pooling=False";
+            SqlConnection objConnection = new SqlConnection(strConnection);
+            objConnection.Open();
+
+            string strCommand = "Update Customer set CustomerName =@CustomerName, Country=@Country, " +
+                "Gender=@Gender,Hobby=@Hobby,Married= @Married WHERE id=@id";
+            SqlCommand objCommand = new SqlCommand(strCommand, objConnection);
+            objCommand.Parameters.AddWithValue("@CustomerName", txtName.Text.Trim());
+            objCommand.Parameters.AddWithValue("@Country", cmbCountry.Text.Trim());
+            objCommand.Parameters.AddWithValue("@Gender", Gender);
+            objCommand.Parameters.AddWithValue("@Hobby", Hobby);
+            objCommand.Parameters.AddWithValue("@Married", Status);
+            objCommand.Parameters.AddWithValue("@id", lblID.Text.Trim());
+            objCommand.ExecuteNonQuery();
+
+            objConnection.Close();
+            clearForm();
+            loadCustomer();
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string strConnection = "Data Source=LAPTOP-76G8DFAG;Initial Catalog=CustomerDB;Integrated Security=True;Pooling=False";
+            SqlConnection objConnection = new SqlConnection(strConnection);
+            objConnection.Open();
+
+
+            string strCommand = "Delete from Customer where id ='" + lblID.Text + "' ";
+            SqlCommand objCommand = new SqlCommand(strCommand, objConnection);
+            objCommand.ExecuteNonQuery();
+            objConnection.Close();
+
+            clearForm();
+            loadCustomer();
+        }
+
+        private void dtgCustomer_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+          
+        }
+    }
+   }
+
